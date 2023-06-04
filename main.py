3d1ab9b72419b62b686d6109937ab2fbc66f77ea
InnaKli <mishka_792000@yahoo.com>
2023-06-04 14:43:05 +0400

    Solution commit

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ from typing import List
 
 
 def foo(nums: List[int]) -> List[int]:
-
-    return
+    result = []
+    for i in range(len(nums)):
+        product = 1
+        for j in range(len(nums)):
+            if i != j:
+                product *= nums[j]
+        result.append(product)
+        print(result)
